c635458893afc3c987020e9259ce7667b1539cf9
milan338 <56327606+milan338@users.noreply.github.com>
2021-09-09 12:22:30 +0930

    Initial config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const path = require('path');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const SRC = path.join(__dirname, 'src');
+const STYLES = path.join(SRC, 'styles');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    webpack: (config, options) => {
+        config.plugins.push(
+            new StyleLintPlugin({
+                configFile: '.stylelintrc',
+                context: STYLES,
+                emitError: true,
+                emitWarning: true,
+            })
+        );
+        return config;
+    },
+    sassOptions: {
+        includePaths: [STYLES],
+    },
+};
