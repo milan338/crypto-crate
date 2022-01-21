0248d30d4b5c33005238e39ee92e981611406258
Milan <milan.338@outlook.com>
2022-01-21 18:32:12 +1030

    Reduce harshness of starfield to improve ui contrast

--- src/components/starfield/Starfield.tsx
+++ src/components/starfield/Starfield.tsx
@@ -1,10 +1,10 @@
 import vert from '@/shaders/components/starfield/starfield.vert';
 import frag from '@/shaders/components/starfield/starfield.frag';
-import { useMemo, useRef, useState } from 'react';
+import { useMemo, useRef, Suspense } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Vector2, RepeatWrapping, RGBFormat, TextureLoader, ShaderMaterial } from 'three';
 import { deg2rad } from '@/util/math';
-import { useWindowSize } from '@/util/hooks/window';
+import { useWindowSize } from '@/hooks/window';
 
 interface StarfieldProps {
     fov: number;
@@ -15,10 +15,13 @@ export default function Starfield(props: StarfieldProps) {
     const ref = useRef<ShaderMaterial>();
     const [windowW, windowH] = useWindowSize();
     // Dimensions of the plane such that it fills the canvas
-    const fov = deg2rad(props.fov);
-    const planeH = 2 * Math.tan(fov / 2) * Math.abs(props.position[2]);
-    const planeW = planeH * (windowW / windowH);
-    // Noise texture to use in shader
+    const { planeW, planeH } = useMemo(() => {
+        const fov = deg2rad(props.fov);
+        const h = 2 * Math.tan(fov / 2) * Math.abs(props.position[2]);
+        const w = h * (windowW / windowH);
+        return { planeW: w, planeH: h };
+    }, [props.fov, props.position, windowH, windowW]);
+    // Noise texture used in starfield shader
     const noiseTexture = useMemo(() => {
         const texture = new TextureLoader().load('/noise.png');
         texture.wrapS = RepeatWrapping;
@@ -33,7 +36,7 @@ export default function Starfield(props: StarfieldProps) {
                 u_time: { value: 3.2 },
                 u_shift: { value: 0.15 },
                 u_speed: { value: 0.05 },
-                u_translatex: { value: 1.4 },
+                u_translatex: { value: 1.55 },
                 u_resolution: { value: new Vector2(windowW, windowH) },
                 u_noise: { value: noiseTexture },
             },
@@ -43,15 +46,17 @@ export default function Starfield(props: StarfieldProps) {
         return shaderMaterial;
     }, [windowW, windowH, noiseTexture]);
     useFrame((state) => {
-        const time = state.clock.getElapsedTime();
         if (!ref.current) return;
+        const time = state.clock.getElapsedTime();
         // Update shader uniforms to progress animation
         ref.current.uniforms.u_time.value = time;
     });
     return (
-        <mesh position={props.position}>
-            <planeGeometry args={[planeW, planeH]} />
-            <shaderMaterial ref={ref} attach="material" args={[starfieldShaderMaterial]} />
-        </mesh>
+        <Suspense fallback={null}>
+            <mesh position={props.position}>
+                <planeGeometry args={[planeW, planeH]} />
+                <shaderMaterial ref={ref} attach="material" args={[starfieldShaderMaterial]} />
+            </mesh>
+        </Suspense>
     );
 }
