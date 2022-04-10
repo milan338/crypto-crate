5b112c9ec23fafacda6251e9e071d95192906ee4
Milan <milan.338@outlook.com>
2022-04-10 14:42:17 +0930

    Improve useScrollPosition performance

--- src/hooks/window.ts
+++ src/hooks/window.ts
@@ -1,4 +1,5 @@
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState, useRef } from 'react';
+import { isServer } from './ssr';
 
 export function useWindowSize() {
     const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
@@ -14,18 +15,35 @@ export function useWindowSize() {
     return size;
 }
 
-export function useScrollPosition(root?: HTMLElement) {
-    const [scrollY, setScrollY] = useState(root ? root.scrollTop : window.scrollY);
+// Get live scroll positions from window
+// Set bool to true to only get updates when element changes from 0 scroll to > 0 scroll
+export function useScrollPosition(debounceDelay: number, bool = false) {
+    const [scrollY, setScrollY] = useState(isServer() ? 0 : window.scrollY);
+    const _scrollY = useRef(scrollY);
+    const wait = useRef(false);
     useEffect(() => {
         const updateScrollY = () => {
-            setScrollY(root ? root.scrollTop : window.scrollY);
+            // Don't rerender component if scrolled state hasn't changed
+            const oldScroll = _scrollY.current;
+            _scrollY.current = window.scrollY;
+            if (bool && !!oldScroll === !!_scrollY.current) return;
+            // Debounce delay - don't delay if scrolled to top, prevent missing scroll state changes
+            if (wait.current && _scrollY.current) return;
+            // Not waiting - wait next times
+            setTimeout(() => {
+                wait.current = false;
+                // Update with new position to avoid missing updates
+                setScrollY(_scrollY.current);
+            }, debounceDelay);
+            wait.current = true;
+            // Update scroll state and rerender component
+            setScrollY(_scrollY.current);
         };
-        if (root) root.addEventListener('scroll', updateScrollY);
-        else window.addEventListener('scroll', updateScrollY);
+        window.addEventListener('scroll', updateScrollY);
         return () => {
             window.removeEventListener('scroll', updateScrollY);
         };
-    }, [root]);
+    }, [debounceDelay, bool]);
     return scrollY;
 }
 
