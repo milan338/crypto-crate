272f7b0c33e262968554b497805c39837ed85a5f
Milan <milan.338@outlook.com>
2022-04-15 22:46:47 +0930

    Add support for multiple crates with a sun each

--- src/components/crate/CrateEffects.tsx
+++ src/components/crate/CrateEffects.tsx
@@ -1,30 +1,43 @@
+import { Fragment } from 'react';
 import { HalfFloatType } from 'three';
 import { EffectComposer, GodRays } from '@react-three/postprocessing';
 import { BlendFunction, Resizer, KernelSize } from 'postprocessing';
 import type { Mesh } from 'three';
 
+export type Suns = Array<Mesh | undefined>;
+export type Keys = Array<string>;
+
 interface CrateEffectsProps {
-    sunRef: Mesh;
+    suns: Suns;
+    keys: Keys;
 }
 
 export default function CrateEffects(props: CrateEffectsProps) {
+    const { suns, keys } = props;
     return (
         // Using a HalfFloatType frame buffer fixes color banding
         <EffectComposer frameBufferType={HalfFloatType}>
-            <GodRays
-                sun={props.sunRef}
-                blendFunction={BlendFunction.SCREEN}
-                width={Resizer.AUTO_SIZE}
-                height={Resizer.AUTO_SIZE}
-                kernelSize={KernelSize.SMALL}
-                samples={30}
-                density={0.96}
-                decay={0.9}
-                weight={0.5}
-                exposure={0.4}
-                clampMax={1}
-                blur={1}
-            />
+            {suns.map((sun, i) => {
+                // Avoid React keys errors for when sun ref is still undefined
+                if (!sun) return <Fragment key={`${keys[i]}-fragment`} />;
+                return (
+                    <GodRays
+                        key={keys[i]}
+                        sun={sun}
+                        blendFunction={BlendFunction.SCREEN}
+                        width={Resizer.AUTO_SIZE}
+                        height={Resizer.AUTO_SIZE}
+                        kernelSize={KernelSize.SMALL}
+                        samples={30}
+                        density={0.96}
+                        decay={0.9}
+                        weight={0.5}
+                        exposure={0.4}
+                        clampMax={1}
+                        blur={1}
+                    />
+                );
+            })}
         </EffectComposer>
     );
 }
