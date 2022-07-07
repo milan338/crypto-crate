d4f87d903ea41f97a7b59a06532666650df74a99
Milan <milan.338@outlook.com>
2022-07-07 13:15:13 +0930

    Remove float text container poping animation

--- src/components/container/FloatTextContainer.tsx
+++ src/components/container/FloatTextContainer.tsx
@@ -1,8 +1,6 @@
 import styles from '@/styles/components/container/FloatTextContainer.module.scss';
 import { useState, useRef } from 'react';
-import { useIntersectionObserver } from '@/hooks/observer';
 import { useHasMounted } from '@/hooks/ssr';
-import { linspace } from '@/util/math';
 import type { ReactNode } from 'react';
 
 export interface FloatTextContainerProps {
@@ -12,42 +10,27 @@ export interface FloatTextContainerProps {
     children?: ReactNode;
 }
 
-// Intersection ratio threshold values to run intersection observer callbacks at
-const thresholds = linspace(10);
-
 export default function FloatTextContainer(props: FloatTextContainerProps) {
     const { heading, subheading, rightAlign, children } = props;
-    const [intRatio, setIntRatio] = useState(0);
-    const [visible, setVisible] = useState(false);
+    const [hovered, setHovered] = useState(false);
     const hasMounted = useHasMounted();
     const ref = useRef<HTMLDivElement>(null);
-    useIntersectionObserver(
-        (event) => {
-            // Stop fading once fully visible
-            if (visible) return;
-            if (event.intersectionRatio === 1) setVisible(true);
-            // Prevent fading out at top of screen - only fade in from bottom
-            if (event.intersectionRect.y > 0) setIntRatio(event.intersectionRatio);
-        },
-        ref.current ?? undefined,
-        { threshold: thresholds }
-    );
     return (
         <div
             ref={ref}
-            className={styles.container}
+            className={`${styles.container} ${hovered ? styles.hovered : ''}`}
             style={
                 hasMounted
                     ? /* eslint-disable indent */
                       {
                           marginLeft: rightAlign ? 'auto' : 0,
                           marginRight: rightAlign ? 0 : 'auto',
-                          opacity: intRatio,
-                          transform: `translateY(${50 * (1 - intRatio)}px)`,
                       }
                     : {}
                 /* eslint-enable indent */
             }
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
         >
             <h2>{subheading}</h2>
             <h1>{heading}</h1>
